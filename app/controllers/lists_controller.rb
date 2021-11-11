ac48954869b597beb077dba91a6766f141505892
manrika malka <manrika.m@gmail.com>
2021-11-11 16:50:43 +0000

    fsg

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,34 @@
+class ListsController < ApplicationController
+  before_action :find_list, only: [:show]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @movies = @list.movies
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(require_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_list
+    @list = List.find(params[:id])
+  end
+
+  def require_params
+    params.require(:list).permit(:name)
+  end
+end
